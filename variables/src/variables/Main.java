babb715c1c5af28463d00e9fae6d00977519f288
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-21 18:43:52 +0300

    java Project

--- variables/src/variables/Main.java
+++ variables/src/variables/Main.java
@@ -3,8 +3,8 @@ package variables;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int sayi=1;// tam sayı değerini tutar.
+	
+		int sayi=1111111;// tam sayı değerini tutar.
 		double sayi2=12.43;// ondalıklı değer tutar.
 		char karakter='F';// tek karekter tutar.
 		String ad="fırat";// metin olarak tutar.
