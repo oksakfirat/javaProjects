3524c3c4c49cc7ec63290b5ce284d18dcb09855b
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-21 23:10:10 +0300

    java Project

--- strings/src/strings/Main.java
+++ strings/src/strings/Main.java
@@ -11,7 +11,22 @@ public class Main {
 		System.out.println(mesaj.concat(" Yaþasýn"));
 		System.out.println(mesaj.startsWith("t"));
 		System.out.println(mesaj.endsWith("l"));
-		char[] karakter= new char[5];
+		char[] karakterler= new char[5];
+		mesaj.getChars(0, 5, karakterler, 0);
+		System.out.println(karakterler);
+		System.out.println(mesaj.indexOf('a'));
+		System.out.println(mesaj.lastIndexOf('e'));
+		
+		String yeniMesaj=mesaj.replace(' ', '-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2,5));
+		for(String kelime:mesaj.split(" ")) {
+			System.out.println(kelime);
+			
+		}
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		System.out.println(mesaj.trim());
 		
 		
 	}
