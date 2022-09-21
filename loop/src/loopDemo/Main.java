fed05299e127db8631727a77cb54b63b0603f2f2
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-21 20:01:52 +0300

    java Project

---
+++ loop/src/loopDemo/Main.java
@@ -0,0 +1,31 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("For döngüsü bitti...");
+		
+	  //While
+		int i=0;
+		while(i<15) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While döngüsü bitti...");
+	  //Do While
+		int j=0;
+		do {
+			System.out.println(j);
+			j++;
+		}while(j<5);
+		System.out.println("Do While döngüsü bitti...");
+		
+		
+
+	}
+
+}
