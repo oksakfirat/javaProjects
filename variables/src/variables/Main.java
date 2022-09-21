32b060c92d23c9484c0d5f16d56b239afd92a4de
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-21 18:42:20 +0300

    java Project

--- variables/src/variables/Main.java
+++ variables/src/variables/Main.java
@@ -7,8 +7,8 @@ public class Main {
 		int sayi=1;// tam sayı değerini tutar.
 		double sayi2=12.43;// ondalıklı değer tutar.
 		char karakter='F';// tek karekter tutar.
-		String ad="fırat";// metin olarak referans tutar.
-		boolean dogrumu=false;// iki değer tutar true false
+		String ad="fırat";// metin olarak tutar.
+		boolean dogruMu=false;// iki değer tutar true false.
 		
 	}
 
