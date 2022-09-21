afdbdd586a9e414b4a563d87b36580cb5bb44154
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-22 00:23:07 +0300

    java Project

---
+++ primeNumber/src/primeNumber/Main.java
@@ -0,0 +1,22 @@
+package primeNumber;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number=46;
+		int counter=0;
+		for(int i=1;i<=number;i++) {
+			if(number%i==0) {
+				counter=counter+1;
+			}
+		}
+		if(counter==2) {
+			System.out.println(number +" "+ "Bir asal sayıdır ");
+		}
+		else {
+			System.out.println(number +" "+"Bir asal sayı değildir");
+		}
+
+	}
+
+}
