21a1d56bd62465eec9ed813d28d48efb96adbf3e
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-21 19:02:12 +0300

    java Project

---
+++ conditinals/src/conditinals/Main.java
@@ -0,0 +1,22 @@
+package conditinals;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		int sayi = 2;
+		if (sayi<2) {
+			System.out.println("sayý 2' den küçüktür");
+			
+		} else if(sayi==2) {
+			System.out.println("sayý 2' ye eþittir. ");
+		}
+		
+		else{
+			System.out.println("sayý 2' den büyüktür");
+		}
+		
+
+	}
+
+}
