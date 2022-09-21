cac6fb649d7602b15e6c0704b3fa375372fc2d26
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-22 00:25:38 +0300

    java Project

--- primeNumber/src/primeNumber/Main.java
+++ primeNumber/src/primeNumber/Main.java
@@ -3,7 +3,7 @@ package primeNumber;
 public class Main {
 
 	public static void main(String[] args) {
-		int number=46;
+		int number=13;
 		int counter=0;
 		for(int i=1;i<=number;i++) {
 			if(number%i==0) {
