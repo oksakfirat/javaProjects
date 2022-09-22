dc0ff39db8fe4ca9c464dd921db7b4230051dd4d
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-22 09:26:54 +0300

    java Project

---
+++ perfectNumber/src/perfectNumber/Main.java
@@ -0,0 +1,23 @@
+package perfectNumber;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number=28;
+		int total=0;
+		for(int i = 1; i < number ; i++) {
+			if(number%i==0) {
+				total=total+i;
+			}
+		}
+		if(total==number) {
+			System.out.println(number+" "+"Mükemmel bir sayýdýr.");
+		}
+		else{
+			System.out.println(number+" "+"Mükemmel bir sayý deðildir.");
+		}
+		
+
+	}
+
+}
